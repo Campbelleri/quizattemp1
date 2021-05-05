1613dd9dec1d2fe6410e5180a905da3079483aa7
Erica Campbell <Campbell.ericac@gmail.com>
2021-05-05 19:16:15 -0400

    first commit

---
+++ student.java
@@ -0,0 +1,55 @@
+package students;
+/**
+ * @TCP
+ */
+
+
+public class student {
+	private String name;
+
+	/**
+	 * 
+	 */
+	public static void main(String[]args) {
+		System.out.println("Test Results");
+		
+		student sl = new student();
+		
+		sl.name = "Shelby";
+		
+		System.out.println("Highest Score " + sl.name);
+		
+		sl.name = "Ben";
+		
+		System.out.println("Highest Score" + sl.name);
+		
+		
+		sl.name = "Sarah";
+		
+		
+		System.out.println("Failed" + sl.name);
+		
+		sl.name ="Erin";
+		
+		System.out.println("Failed" + sl.name);
+		
+		
+		System.out.println("Two students failed");
+		
+		System.out.println("Two students got a C");
+		
+		System.out.println("Two students got a B");
+		System.out.println("Four students got a A");
+		
+		
+		
+		
+		
+		
+	}
+
+	
+	
+	
+			
+ }
